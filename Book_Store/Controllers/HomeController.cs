f48b5982ad4c57c689cb439e9e87e47d95729022
Sebastian Perez <jpere723@fiu.edu>
2019-02-01 15:07:14 -0500

    Basic layout with links

--- Book_Store/Controllers/HomeController.cs
+++ Book_Store/Controllers/HomeController.cs
@@ -15,9 +15,37 @@ namespace Book_Store.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Browsing()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "This is the browsing.";
+
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            ViewData["Message"] = "This is the book details.";
+
+            return View();
+        }
+
+        public IActionResult Ratings()
+        {
+            ViewData["Message"] = "This is the book rating.";
+
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            ViewData["Message"] = "This is the shopping cart.";
+
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            ViewData["Message"] = "This is the profile management.";
 
             return View();
         }
