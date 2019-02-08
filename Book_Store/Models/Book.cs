6c995ae506a6de3f3923518284331aab878ee5e5
Danny Perez <dpere361@fiu.edu>
2019-02-07 19:37:35 -0500

    Begin DB Establishment

---
+++ Book_Store/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Store.Models
+{
+    public class Book
+    {
+    }
+}
--- Book_Store/Program.cs
+++ Book_Store/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace Book_Store
 {
@@ -15,6 +17,47 @@ namespace Book_Store
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "<cen4010group6.database.windows.net>";
+                builder.UserID = "<Group6>";
+                builder.Password = "<Cen401019>";
+                builder.InitialCatalog = "<your_database>";
+                
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    /*
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
+                    sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+                    sb.Append("JOIN [SalesLT].[Product] p ");
+                    sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                    */
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
